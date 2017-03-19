c5f3b9898f54a9c5d57f151b031f7b9e0eb0018b
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-03-19 20:43:42 +0000

    Enemy AI v.2 Kinda follows

--- Assets/Scripts/BasicEnemyAI.cs
+++ Assets/Scripts/BasicEnemyAI.cs
@@ -6,38 +6,61 @@ public class BasicEnemyAI : MonoBehaviour {
 
     public Transform playerTarget;
     public float movementSpeed;
-    //private bool hasLanded;
+    private bool hasLanded;
+    private bool hasDirection;
+    //private Time lastDeltaTime;
+    private int jumpDirection;
 
 	// Use this for initialization
 	void Start () {
         playerTarget = GameObject.FindWithTag("Player").transform;
-        //hasLanded = true;
-	}
+        hasLanded = true;
+        hasDirection = true;
+        jumpDirection = randomDirectionInt();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         // Keeps track of the enemies current position and changes it based on the location of the playerTarget AKA The Player
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(playerTarget.position.x, playerTarget.position.y), movementSpeed * Time.deltaTime);
 
-        Debug.Log("Delta Time == " + Time.deltaTime);
-
-        if (playerTarget.position.y > (transform.position.y + 2)) {
-            randomDirection();
-        }
-        else {
-            if (playerTarget.position.x > transform.position.x) {
-                transform.localScale = new Vector3(-1, 1, 1);
+        //Debug.Log("Delta Time == " + Time.deltaTime);
+        if(hasLanded) {
+            if (playerTarget.position.y > (transform.position.y + 2)) {
+                hasLanded = false;
+                transform.localScale = new Vector3(jumpDirection, 1, 1);
+            } else {
+                hasLanded = true;
+                if (playerTarget.position.x > transform.position.x) {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                } else if (playerTarget.position.x < transform.position.x) {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
             }
-            else if (playerTarget.position.x < transform.position.x) {
-                transform.localScale = new Vector3(1, 1, 1);
-            }
+        } else {
+            transform.localScale = new Vector3(jumpDirection, 1, 1);
         }
     }
 
-    void randomDirection() {
+    private void randomDirection() {
         float randomNumber = Random.Range(-1.5f, 1.0f);
         int direction = (int)randomNumber;
-        Debug.Log("Direction is :" + direction);
+        
         transform.localScale = new Vector3(direction, 1, 1);
+    }
+
+    private int randomDirectionInt() {
+        float randomNumber = Random.Range(-1.0f, 1.0f);
+        int direction = (int)randomNumber;
+        while (direction == 0) {
+            randomNumber = Random.Range(-1.0f, 1.0f);
+            direction = (int)randomNumber;
+        }
+        
+        Debug.Log("Direction is :" + direction);
+        return direction;
+        //return direction;
+        //Debug.Log("Direction is :" + direction);
+        //transform.localScale = new Vector3(direction, 1, 1);
     }
 }
