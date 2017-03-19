4590e7a65fc2c44cd75c81d983db277f5a64475f
x14130114 <x14130114@student.ncirl.ie>
2017-03-19 23:47:33 +0000

    Bullet and Score

---
+++ Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBase : MonoBehaviour {
+    public Transform playerTarget;
+    public int hitPoints = 1;
+    public float movementSpeed = 1f;
+    
+
+	// Use this for initialization
+	public void Start () {
+        playerTarget = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    public void Update () {
+        // Keeps track of the enemies current position and changes it based on the location of the playerTarget AKA The Player
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(playerTarget.position.x, playerTarget.position.y), movementSpeed * Time.deltaTime);
+
+    }
+}
